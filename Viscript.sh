2ae3479c9e824894a4b5ca229fdbe98d122bf023
jivko <rott@mail.bg>
2014-11-20 08:32:49 +0200

    Viscript.sh
    This script is update metasploit-framework exploits

---
+++ Viscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cat <<EOF
+*****************
+ Vi Script
+Update Metasploit
+*****************
+by Gie Cloudnine
+EOF
+echo && echo "Press Enter To Continue ${endc}"
+  read input
+    cd /usr/share/exploitdb
+    rm -rf archive.tar.bz2
+    wget http://www.exploit-db.com/archive.tar.bz2
+    tar xvfj archive.tar.bz2
+    rm -rf archive.tar.bz2
+    echo -e "Done"
+    sleep 3
