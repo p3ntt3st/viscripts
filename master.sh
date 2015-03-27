7bc59b21af016969327ae0253976871ce1518341
Zhivko <rott@mail.bg>
2015-03-27 10:31:23 +0200

    master.sh
    Upgrade to Vibuntu
    
    this is alpha shell

---
+++ master.sh
@@ -0,0 +1,45 @@
+echo ""
+echo "#####################################################"
+echo " by : Master vi |chyyk@hotmail.com"
+echo"           Vibuntu Team
+echo"     your questions   Vibuntu support 
+echo "#####################################################"
+echo ""
+echo ""
+apt-get update && apt-get upgrade
+echo""
+echo " Installation and updating resources"
+echo ""
+apt-get install gnome-session-fallback
+echo ""
+apt-get install Compiz
+echo ""
+apt-get clean && apt-get autoclean 
+echo""
+echo "Add Kali linux official repositories "
+add-apt-repository "deb http://http.kali.org/kali kali main non-free" 
+echo""
+add-apt-repository "deb http://security.kali.org/kali-security kali/updates main  non-free"
+echo""
+add-apt-repository  "deb-src http://http.kali.org/kali kali main non-free "
+echo""
+add-apt-repository  "deb-src http://security.kali.org/kali-security kali/updates main  non-free"
+echo ""
+apt-get update 
+echo ""
+apt-get install kali-archive-keyring
+echo ""
+apt-get update 
+echo ""
+apt-get install kali-menu 
+echo ""
+echo ""
+apt-get install nmap zenmap wireshark wifite sqlmap metasploit armitage 
+echo " "
+echo ""
+echo ""
+echo ""
+echo ""
+echo "
+echo "
+echo "by : Master Vi | chyyk@hotmail.com"
